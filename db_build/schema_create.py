048d9ed666a5567f61bba84b5eac53ae56135eb4
lsanford <lynn.sanford@colorado.edu>
2024-09-21 16:13:22 -0600

    Updated schema

--- db_build/schema_create.py
+++ db_build/schema_create.py
@@ -25,19 +25,36 @@ import dbutils
 
 # Load config file and connect to db
 config = dbutils.load_config(
-    "/Shares/dbnascent/DBNascent-build/config/config_build.txt"
+    "/home/lsanford/DBNascent-build/config/config_build.txt"
 )
 db_url = config["file_locations"]["database"]
 creds = config["file_locations"]["credentials"]
 dbconnect = dbutils.dbnascentConnection(db_url, creds)
 
 # create the pydot graph object by autoloading all tables via a bound metadata object
-graph = create_schema_graph(metadata=MetaData(dbconnect.engine),
+graph = create_schema_graph(
+    tables = [
+      dborm.searchEquiv.__table__,
+      dborm.sampleEquiv.__table__,
+      dborm.linkIDs.__table__,
+      dborm.samples.__table__,
+      dborm.papers.__table__,
+      dborm.genetics.__table__,
+      dborm.organisms.__table__,
+      dborm.conditions.__table__,
+      dborm.tissues.__table__,
+      dborm.bidirs.__table__,
+      dborm.nascentflowRuns.__table__,
+      dborm.bidirflowRuns.__table__,
+      dborm.conditionLink.__table__,
+      dborm.nascentflowLink.__table__,
+      dborm.bidirflowLink.__table__,
+    ],
     show_datatypes=False, # The image would get nasty big if we'd show the datatypes
     show_indexes=False, # ditto for indexes
     rankdir='LR', # From left to right (instead of top to bottom)
-    concentrate=False # Don't try to join the relation lines together
+    concentrate=False, # Don't try to join the relation lines together
 )
-graph.write_png("/Shares/dbnascent/DBNascent-build/dbschema.png")
+graph.write_png("/home/lsanford/DBNascent-build/dbschema.png")
 
 # schema_create.py ends here
